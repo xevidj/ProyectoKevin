dc36f45beb9dd20a14e5bc1f34f115535a24b0e0
Kevin <kevin7sharely@gmail.com>
2018-10-21 18:36:42 -0500

    Update figura.php

--- figura.php
+++ figura.php
@@ -1,20 +1,12 @@
-<html>
-<head>
-<title>Figura</title>
-</head>
-<body>
-<?php
-	//Tamaño de la línea más larga, la de enmedio de ambos triángulos
-$num = 8;
-	//bucle para el triángulo superior
-for($n=1; $n <= $num; $n++){
-    echo str_pad('', $n, '*') . '/n';
-}
-//bucle para el triángulo invertido
-for($n=$num - 1; $n >= 1; $n--){
-    echo str_pad('', $n, '*') . '/n';
-}
-?>
-<p><a href="javascript:history.go(-1)">Back</a></p>
-</body>
-</html>
+
+<?php 
+     
+   $n1=$_GET['numero']; 
+    
+   for ($b=1;$b<=$n1;$b++) 
+   {for ($a=1;$a<=$b;$a++) 
+   {echo '*';} 
+    echo '/n'; 
+   } 
+
+  ?>
