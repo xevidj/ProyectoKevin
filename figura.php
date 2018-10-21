2aa36f4ee4553c0f83c7d8073d36e2292dded51f
Kevin <kevin7sharely@gmail.com>
2018-10-21 18:25:48 -0500

    Update figura.php

--- figura.php
+++ figura.php
@@ -8,11 +8,11 @@
 $num = 8;
 	//bucle para el triángulo superior
 for($n=1; $n <= $num; $n++){
-    echo str_pad('', $n, '*') . '<br />';
+    echo str_pad('', $n, '*') . '/n';
 }
 //bucle para el triángulo invertido
 for($n=$num - 1; $n >= 1; $n--){
-    echo str_pad('', $n, '*') . '<br />';
+    echo str_pad('', $n, '*') . 'n';
 }
 ?>
 <p><a href="javascript:history.go(-1)">Back</a></p>
