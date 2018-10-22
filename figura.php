80241e8460f1a4b595af6e40db3eba571b0abc9c
Kevin <kevin7sharely@gmail.com>
2018-10-21 23:31:47 -0500

    Update figura.php

--- figura.php
+++ figura.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
- <head>
+ <head><!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="css/default/default.css" type="text/css" media="screen" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+		<script src="jquery.nivo.slider.pack.js" type="text/javascript"></script>
   <title>Prueba de PHP</title>
  </head>
  <meta charset="UTF-8">
