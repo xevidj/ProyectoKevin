5180fe98c54a1b5bc453d473286384bdce44cdf2
Kevin <kevin7sharely@gmail.com>
2018-11-10 19:03:51 -0500

    sdasd

--- enviar.php
+++ enviar.php
@@ -4,6 +4,10 @@
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
+$ciudad = $_POST['ciudad'];
+$provincia = $_POST['contact-provincia_id'];
+$canton = $_POST['contact-canton_id'];
+$parroquia = $_POST['contact-parroquia_id'];
 $mensaje = $_POST['mensaje'];
 
 // Datos para el correo
@@ -13,6 +17,10 @@ $asunto = "Contacto desde nuestra web";
 $carta = "De: $nombre \n";
 $carta .= "Correo: $correo \n";
 $carta .= "Telefono: $telefono \n";
+$carta .= "Ciudad: $ciudad \n";
+$carta .= "Provincia: $provincia \n";
+$carta .= "Canton: $canton \n";
+$carta .= "Parrouia: $parroquia \n";
 $carta .= "Mensaje: $mensaje";
 
 // Enviando Mensaje
