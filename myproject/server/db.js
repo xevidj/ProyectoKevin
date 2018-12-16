b79c274063692f06f4caf73e5fc6e84aa55096fb
Kevin <kevin7sharely@gmail.com>
2018-12-16 17:51:40 -0500

    s

--- myproject/server/db.js
+++ myproject/server/db.js
@@ -15,7 +15,7 @@ const articleSchema = new Schema({
 })
 
 //连接blog数据库
-mongoose.connect('mongodb://127.0.0.1:27017/blog',{
+mongoose.connect('mongodb://127.0.0.1:27017/myproject',{
   useMongoClient: true
 })
 
