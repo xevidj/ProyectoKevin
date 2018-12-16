c8afcff8677b9fa5bc295fd4a80ca40878598f3c
Kevin <kevin7sharely@gmail.com>
2018-12-16 14:06:40 -0500

    s

--- proyecto/src/components/HelloWorld.vue
+++ proyecto/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-    Bieido / customize this project,<br>
+    Bieido / cdasd,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
