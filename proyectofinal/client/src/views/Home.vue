19732ce26318cab8d6a09ec50a95ab2e85784b25
Kevin <kevin7sharely@gmail.com>
2018-12-16 21:27:34 -0500

    s

---
+++ proyectofinal/client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <Messages/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Messages from '@/components/Messages.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Messages
+  }
+}
+</script>
