b465f59d17cd199d455377b284b782b69f4018b9
Kevin <kevin7sharely@gmail.com>
2018-12-19 16:30:12 -0500

    s

--- proyectofinal/client/src/components/AppNavbar.vue
+++ proyectofinal/client/src/components/AppNavbar.vue
@@ -2,7 +2,7 @@
   <v-toolbar
     app>
     <v-toolbar-title>
-      <v-btn flat :to="{ name: 'home' }">Trello Clone</v-btn>
+      <v-btn flat :to="{ name: 'home' }">Torosina Blog's</v-btn>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items v-if="!user">
@@ -26,4 +26,3 @@ export default {
   },
 };
 </script>
-
