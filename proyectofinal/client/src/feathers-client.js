b6810b0d1b5cf9423024966528096fcccb84c5e2
Kevin <kevin7sharely@gmail.com>
2018-12-19 16:33:50 -0500

    s

--- proyectofinal/client/src/feathers-client.js
+++ proyectofinal/client/src/feathers-client.js
@@ -3,7 +3,7 @@ import socketio from '@feathersjs/socketio-client';
 import auth from '@feathersjs/authentication-client';
 import io from 'socket.io-client';
 
-let API_URL = 'http://104.248.237.254:3030';
+let API_URL = 'https://cg-trello-clone.now.sh';
 
 if (window.location.hostname === '104.248.237.254:8081') {
   API_URL = 'http://104.248.237.254:8081:3030';
