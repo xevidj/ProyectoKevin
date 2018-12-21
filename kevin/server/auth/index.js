10c34003f3ebb4373c5d3411fb3725704261ec40
Kevin <kevin7sharely@gmail.com>
2018-12-21 01:12:00 -0500

    s

---
+++ kevin/server/auth/index.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('../db/connection');
+const users = db.get('users');
+users.createIndex('username', { unique: true });
+
+const router = express.Router();
+
+const schema = Joi.object().keys({
+  username: Joi.string().regex(/(^[a-zA-Z0-9_]+$)/).min(2).max(30).required(),
+  password: Joi.string().trim().min(10).required()
+});
+
+// const createTokenSendResponse = function(user, res, next) {
+// const createTokenSendResponse = (user, res, next) => {
+function createTokenSendResponse(user, res, next) {
+  const payload = {
+    _id: user._id,
+    username: user.username
+  };
+
+  jwt.sign(payload, process.env.TOKEN_SECRET, {
+    expiresIn: '1d'
+  }, (err, token) => {
+    if (err) {
+      respondError422(res, next);
+    } else {
+      res.json({
+        token
+      });
+    }
+  });
+}
+
+// any route in here is pre-pended with /auth
+router.get('/', (req, res) => {
+  res.json({
+    message:  '🔐'
+  });
+});
+
+router.post('/signup', (req, res, next) => {
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    users.findOne({
+      username: req.body.username
+    }).then(user => {
+      // if user is undefined, username is not in the db, otherwise, duplicate user detected
+      if (user) {
+        // there is already a user in the db with this username...
+        // respond with an error!
+        const error = new Error('That username is not OG. Please choose another one.');
+        res.status(409);
+        next(error);
+      } else {
+        // hash the password
+        bcrypt.hash(req.body.password.trim(), 12).then(hashedPassword => {
+          // insert the user with the hashed password
+          const newUser = {
+            username: req.body.username,
+            password: hashedPassword
+          };
+
+          users.insert(newUser).then(insertedUser => {
+            createTokenSendResponse(insertedUser, res, next);
+          });
+        });
+      }
+    });
+  } else {
+    res.status(422);
+    next(result.error);
+  }
+});
+
+function respondError422(res, next) {
+  res.status(422);
+  const error = new Error('Unable to login.');
+  next(error);
+}
+
+router.post('/login', (req, res, next) => {
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    users.findOne({
+      username: req.body.username,
+    }).then(user => {
+      if (user) {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((result) => {
+            if (result) {
+              createTokenSendResponse(user, res, next);
+            } else {
+              respondError422(res, next);
+            }
+          });
+      } else {
+        respondError422(res, next);
+      }
+    });
+  } else {
+    respondError422(res, next);
+  }
+});
+
+module.exports = router;
--- kevin/server/index.js
+++ kevin/server/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const volleyball = require('volleyball');
+const cors = require('cors');
 
+require('dotenv').config();
 
 const app = express();
 
+const middlewares = require('./auth/middlewares');
 // const auth = require('./auth/index.js');
 // const auth = require('./auth/index');
+const auth = require('./auth');
+const notes = require('./api/notes');
 
 app.use(volleyball);
-
+app.use(cors({
+  origin: 'http://localhost:8081'
+}));
+app.use(express.json());
+app.use(middlewares.checkTokenSetUser);
 
 app.get('/', (req, res) => {
   res.json({
     message: '🦄🌈✨Hello World! 🌈✨🦄',
+    user: req.user,
   });
 });
 
+app.use('/auth', auth);
+app.use('/api/v1/notes', middlewares.isLoggedIn, notes);
 
 function notFound(req, res, next) {
   res.status(404);
@@ -34,7 +46,7 @@ function errorHandler(err, req, res, next) {
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
