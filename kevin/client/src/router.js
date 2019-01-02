7434763dc820c4ede857afc98390e7cdb87c057a
Kevin <kevin7sharely@gmail.com>
2019-01-02 01:27:29 -0500

    s

--- kevin/client/src/router.js
+++ kevin/client/src/router.js
@@ -1,12 +1,30 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Signup from './views/Signup.vue';
+import Login from './views/Login.vue';
+import Dashboard from './views/Dashboard.vue';
 
 Vue.use(Router);
 
+function loggedInRedirectDashboard(to, from, next) {
+  if (localStorage.token) {
+    next('/dashboard');
+  } else {
+    next();
+  }
+}
+
+function isLoggedIn(to, from, next) {
+  if (localStorage.token) {
+    next();
+  } else {
+    next('/login');
+  }
+}
+
+
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
@@ -14,12 +32,22 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
+      beforeEnter: loggedInRedirectDashboard,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      beforeEnter: loggedInRedirectDashboard,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      beforeEnter: isLoggedIn,
     },
   ],
 });
