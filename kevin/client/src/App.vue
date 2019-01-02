4b951368b8282790b9d1ecd921bb46f553dfd40c
Kevin <kevin7sharely@gmail.com>
2019-01-02 01:11:31 -0500

    s

--- kevin/client/src/App.vue
+++ kevin/client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Auth for Newbs</a>
+      <a class="navbar-brand" href="#">BLOG</a>
     </nav>
     <router-view class="container pt-2" />
   </div>
