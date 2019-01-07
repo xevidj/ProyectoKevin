e0d5436fe8b121c07e86c0d68ecab35eb39ca48d
Kevin <kevin7sharely@gmail.com>
2019-01-07 15:00:09 -0500

    s

--- kevin/server/db/connection.js
+++ kevin/server/db/connection.js
@@ -1,6 +1,6 @@
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('mongodb://auth-for-noobs:1234@localhost:27017/auth-for-noobs');
+var db = monk('mongodb://mongoAdmin:changeMe@localhost:27017/auth-for-noobs');
 
 
 module.exports = db;
