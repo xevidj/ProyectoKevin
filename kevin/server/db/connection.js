fd1d7685ba2476b11121efe22f98774555fccf6a
Kevin <kevin7sharely@gmail.com>
2019-01-07 14:57:21 -0500

    s

--- kevin/server/db/connection.js
+++ kevin/server/db/connection.js
@@ -2,12 +2,5 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('mongodb://auth-for-noobs:1234@localhost:27017/auth-for-noobs');
 
-var app = express();
-
-// Make our db accessible to our router
-app.use(function(req,res,next){
-        req.db = db;
-        next();
-});
 
 module.exports = db;
