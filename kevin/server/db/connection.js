2035966e8e718d78052d52104c2ba19255dfafd8
Kevin <kevin7sharely@gmail.com>
2019-01-07 14:46:30 -0500

    s

--- kevin/server/db/connection.js
+++ kevin/server/db/connection.js
@@ -1,4 +1,19 @@
-const monk = require('monk');
-const db = monk('localhost/auth-for-noobs');
-MONGO_URL=mongodb://iwall:testpw@localhost:27017/admin;
-module.exports = db;
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+const connectToMongoDB = () => {
+  const opts = {
+    keepAlive: true,
+    keepAliveInitialDelay: 300000,
+    socketTimeoutMS: 30000,
+    poolSize: 50,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    autoReconnect: true,
+  };
+
+  return mongoose.connect('mongodb://admin:changeMe@mongodb:27017/auth-for-noobs', opts);
+};
+
+export default connectToMongoDB;
